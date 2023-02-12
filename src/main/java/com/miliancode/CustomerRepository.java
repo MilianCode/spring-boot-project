bfdb1f3a8f777707e521696fef9e00a4bf3e82b3
Maksym <milian.code@gmail.com>
2023-02-12 03:00:20 +0100

    https://github.com/MilianCode/spring-boot-project

---
+++ src/main/java/com/miliancode/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.miliancode;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer> {
+
+
+}
